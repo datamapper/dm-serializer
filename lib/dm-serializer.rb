b3423e5c835c6501ab7ae3547ee51137802c7a4a
snusnu <gamsnjaga@gmail.com>
2010-06-06 19:09:46 +0200

    Removed duplicate constant aliasing
    The same gets done in dm-serializer/common.rb which
    gets required by all the various to_* serializers.
    
    In general, it might not even be necessary to do
    any of these aliasings at all in plugin land, since
    dm-core actually does it itself already, and needs
    to be required before the plugins do their thing
    anyway (most of the times at least).
    
    I'm not sure if this change will actually eliminate
    all const redefinition warnings, but executing this
    code in that place is definitely not necessary.
    
    [#1302]

--- lib/dm-serializer.rb
+++ lib/dm-serializer.rb
@@ -1,12 +1,3 @@
-begin
-  require 'active_support/ordered_hash'
-rescue LoadError
-  require 'extlib/dictionary'
-  module ActiveSupport
-    OrderedHash = Dictionary
-  end
-end
-
 require 'dm-serializer/to_json'
 require 'dm-serializer/to_xml'
 require 'dm-serializer/to_yaml'
